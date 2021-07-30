e35ae0401bec48155742d483b9efe89e58213822
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-07-30 14:04:02 +0300

    add a constraint to the 'team_status' field in the 'teams' table

--- src/main/resources/db/changelog/v-1.0/10-create-teams-table.sql
+++ src/main/resources/db/changelog/v-1.0/10-create-teams-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE teams (
     team_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL UNIQUE,
-    team_status ENUM('ACTIVE','DELETED'),
+    team_status ENUM('ACTIVE','DELETED') NOT NULL DEFAULT 'ACTIVE',
     PRIMARY KEY(team_id)
 ) ENGINE=INNODB
 GO
